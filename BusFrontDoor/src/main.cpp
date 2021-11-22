13de79401837bc8dbf07d2bc38cbfd69bd8d3ba9
Lewin2 <faith2296@naver.com>
2021-11-22 11:45:00 +0900

    file update

--- BusFrontDoor/src/main.cpp
+++ BusFrontDoor/src/main.cpp
@@ -1,9 +1,55 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+#include <Servo.h>
+
+SoftwareSerial mySoftwareSerial(5, 6);
+DFRobotDFPlayerMini myDFPlayer;
+Servo servo;
+
+const int pinServo = 3;
+
+char buf;
+volatile boolean done = false;
 
 void setup() {
-  // put your setup code here, to run once:
+  mySoftwareSerial.begin(9600);
+  Serial.begin(9600);
+
+  servo.attach(pinServo);
+  servo.write(0);
+
+  pinMode(MISO,OUTPUT);
+  SPCR |= _BV(SPE);
+  SPCR |= _BV(SPIE);
+  SPCR &= ~_BV(MSTR);
+  if (!myDFPlayer.begin(mySoftwareSerial))
+  {
+    Serial.println(F("DF Player 오류"));
+    while (true)
+    {
+      delay(0);
+    }
+  }
+  myDFPlayer.volume(30);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(done){
+    Serial.print(buf);
+    if(buf == '1'){
+      servo.write(180);
+      myDFPlayer.play(1);
+    }
+    else if(buf == '0'){
+      servo.write(0);
+    }
+     done = false;
+  }
+}
+
+ISR(SPI_STC_vect) {
+  byte c = SPDR;
+  buf = c;
+  done = true;
 }
